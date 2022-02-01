91631e8f52ba49e81b84249be13f5c47b2cdb043
Théo Mielnikoff <theo.mielnikoff@gmail.com>
2022-02-01 14:51:43 +0100

    Update

--- src/app/partage/formulaire/formulaire.component.ts
+++ src/app/partage/formulaire/formulaire.component.ts
@@ -67,19 +67,6 @@ export class FormulaireComponent implements OnInit {
     this.musicModel.style!.splice(index, 1);
   }
 
-  onFileSelected(event:Event | null) {
-    const files = (<HTMLInputElement>event?.currentTarget).files;
-    const file:File | null = files!.item(0);
-
-    if (file) {
-        let reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = (_event) => {
-          this.musicModel.picture = reader.result;
-        }
-    }
-  }
-
   /**
    * Fonction pour construire notre formulaire
    * @returns {FormGroup}
